89e80b99bbdcf740e34e114252e4709df098f640
arnaudjolivier <81567917+arnaudjolivier@users.noreply.github.com>
2023-01-18 12:52:02 +0100

    push debut code de merde
    vincent cest pa codé

--- Source/TPSProjet5/Private/Chest.cpp
+++ Source/TPSProjet5/Private/Chest.cpp
@@ -3,43 +3,65 @@
 
 #include "Chest.h"
 
+#include "Engine/Engine.h"
+
+
 // Sets default values
 AChest::AChest()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-	VisualMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Chest"));
-	VisualMesh->SetupAttachment(RootComponent);
+    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
+    VisualMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Chest"));
+    VisualMesh->SetupAttachment(RootComponent);
 
-	static ConstructorHelpers::FObjectFinder<UStaticMesh> CubeVisualAsset(TEXT("/Game/StarterContent/Shapes/Shape_Cube.Shape_Cube"));
+    BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Collision"));
 
-	if (CubeVisualAsset.Succeeded())
-	{
-		VisualMesh->SetStaticMesh(CubeVisualAsset.Object);
-		VisualMesh->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
-	}
+    BoxCollision->SetCollisionProfileName(TEXT("Trigger"));
+
+    BoxCollision->OnComponentBeginOverlap.AddDynamic(this, &AChest::OnOverlapBegin);
+    BoxCollision->OnComponentEndOverlap.AddDynamic(this, &AChest::OnOverlapEnd);
+
+
+
+    static ConstructorHelpers::FObjectFinder<UStaticMesh> CubeVisualAsset(TEXT("/Game/AncientTreasures/Meshes/SM_Chest_02c.SM_Chest_02c"));
+
+    if (CubeVisualAsset.Succeeded())
+    {
+        VisualMesh->SetStaticMesh(CubeVisualAsset.Object);
+        BoxCollision->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
+        VisualMesh->SetWorldScale3D(FVector(0.5f));
+    }
+
 }
-
-
 
 // Called when the game starts or when spawned
 void AChest::BeginPlay()
 {
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
+
 }
 
 // Called every frame
+// Called every frame
 void AChest::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
-	FVector NewLocation = GetActorLocation();
-	FRotator NewRotation = GetActorRotation();
-	float RunningTime = GetGameTimeSinceCreation();
-	float DeltaHeight = (FMath::Sin(RunningTime + DeltaTime) - FMath::Sin(RunningTime));
-	NewLocation.Z += DeltaHeight * 20.0f;       //Scale our height by a factor of 20
-	float DeltaRotation = DeltaTime * 20.0f;    //Rotate by 20 degrees per second
-	NewRotation.Yaw += DeltaRotation;
-	SetActorLocationAndRotation(NewLocation, NewRotation);
+    Super::Tick(DeltaTime);
+    FVector NewLocation = GetActorLocation();
+    FRotator NewRotation = GetActorRotation();
+    float RunningTime = GetGameTimeSinceCreation();
+    float DeltaHeight = (FMath::Sin(RunningTime + DeltaTime) - FMath::Sin(RunningTime));
+    NewLocation.Z += DeltaHeight * 20.0f;       //Scale our height by a factor of 20
+    float DeltaRotation = DeltaTime * 20.0f;    //Rotate by 20 degrees per second
+    NewRotation.Yaw += DeltaRotation;
+    SetActorLocationAndRotation(NewLocation, NewRotation);
+}
 
+void AChest::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Overlap Begin"));
+}
+
+void AChest::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+    GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Overlap End"));
 }
--- Source/TPSProjet5/Public/Chest.h
+++ Source/TPSProjet5/Public/Chest.h
@@ -4,26 +4,37 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
 #include "Chest.generated.h"
+
 
 UCLASS()
 class TPSPROJET5_API AChest : public AActor
 {
-	GENERATED_BODY()
-	UPROPERTY(VisibleAnywhere)
-	UStaticMeshComponent* VisualMesh;
-	
-public:	
-	// Sets default values for this actor's properties
-	AChest();
+    GENERATED_BODY()
+        UPROPERTY(VisibleAnywhere) UStaticMeshComponent* VisualMesh;
+
+
+
+public:
+    // Sets default values for this actor's properties
+    AChest();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-	
+public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+
+    UPROPERTY(EditAnywhere, Category = "Collision") UBoxComponent* BoxCollision;
+
+    UFUNCTION()
+        void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+    UFUNCTION()
+        void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
 };
